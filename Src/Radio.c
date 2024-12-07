cbfb4b2c1723768de7bf1775a153f35e02ed652c
wataoxp <168703313+wataoxp@users.noreply.github.com>
2024-12-07 15:13:42 +0900

    Update Radio.c
    Structure for RDA5807Register

--- Src/Radio.c
+++ Src/Radio.c
@@ -2,65 +2,89 @@
 #include "main.h"
 #include "ll_i2c.h"
 
+typedef struct{
+	uint16_t Config;
+	uint16_t Tuning;
+	uint16_t GpioSet;
+	uint16_t VolumeSet;
+}RDA_Typedef;
+
+static RDA_Typedef RadioReg = {0};
+
+static inline void SetRegister(I2C_TypeDef *I2Cx,uint8_t reg,uint16_t value)
+{
+	uint8_t WriteBuffer[2] ={
+			(value >> 8),(value & 0xFF),
+	};
+	StreamI2C_Mem_Write(I2Cx, RADIO_ADDR_PUSH, WriteBuffer, reg, I2C_MEMADD_SIZE_8BIT, sizeof(WriteBuffer));
+}
 void RadioInit(I2C_TypeDef *I2Cx)
 {
-	uint16_t config = FLG_DHIZ | FLG_DMUTE | FLG_BASS
-				| FLG_NEW | FLG_ENABLE;
+	RadioReg.Config = FLG_DHIZ | FLG_DMUTE | FLG_BASS | FLG_NEW | FLG_ENABLE;
+	RadioReg.Tuning = BAND_WORLD;
+	RadioReg.GpioSet = FLG_DE | FLG_STC | FLG_SOFT_MUTE | GPIO2_INT_OFF;
+	RadioReg.VolumeSet = FLG_LNA | FLG_INT_ENABLE | (0x4 << THRESHOLD_SHIFT) | VOLUME_MASK;
+
 	uint8_t init[] = {
-			(config >> 8), (config & 0xff),
-			0x00,BAND_WORLD,
-			(GPIO_INT_OFF >> 8), (GPIO_INT_OFF & 0xff),
-			(VOLUME_DEFAULT >> 8), (VOLUME_DEFAULT & 0xff),};
-
+			(RadioReg.Config >> 8),(RadioReg.Config & 0xFF),
+			(RadioReg.Tuning >> 8),(RadioReg.Tuning & 0xFF),
+			(RadioReg.GpioSet >> 8),(RadioReg.GpioSet & 0xFF),
+			(RadioReg.VolumeSet >> 8),(RadioReg.VolumeSet & 0xFF),
+	};
 	I2C_Master_Transmit(I2Cx, RADIO_ADDR_SEQ, init, sizeof(init));
-}
-void RadioTune(I2C_TypeDef *I2Cx,uint16_t chan)
-{
-	uint16_t tune = (chan << CHAN_SHIFT) | BAND_WORLD | FLG_TUNE;
-	uint8_t tune_buf[2] = {(uint8_t)(tune >> 8 ),tune & 0xff};
-
-	StreamI2C_Mem_Write(I2Cx, RADIO_ADDR_PUSH, tune_buf, REG_TUNING, I2C_MEMADD_SIZE_8BIT, sizeof(tune_buf));
 }
 void Seek(I2C_TypeDef *I2Cx ,uint16_t seekmode)
 {
-	uint8_t gpio_buf[2] = {(GPIO_INT_ON >> 8),GPIO_INT_ON & 0xff};
-	uint16_t SeekConfig = FLG_DHIZ | FLG_DMUTE | FLG_BASS | seekmode | FLG_SEEK | FLG_NEW | FLG_ENABLE;
-	uint8_t seek_buf[2] = {(uint8_t)(SeekConfig >> 8),SeekConfig & 0xff};
+	RadioReg.GpioSet = (RadioReg.GpioSet & ~GPIO2_INT_MASK) | GPIO2_INT_ON;
+	RadioReg.Config = (RadioReg.Config & ~(FLG_SEEKUP | FLG_SEEK)) | FLG_SEEK | seekmode;
+
+	SetRegister(I2Cx, REG_GPIO,RadioReg.GpioSet);
+	SetRegister(I2Cx, REG_CONFIG,RadioReg.Config);
 
 	__disable_irq();
 
-	StreamI2C_Mem_Write(I2Cx, RADIO_ADDR_PUSH, gpio_buf, REG_GPIO, I2C_MEMADD_SIZE_8BIT, sizeof(gpio_buf));
-	StreamI2C_Mem_Write(I2Cx, RADIO_ADDR_PUSH, seek_buf, REG_CONFIG, I2C_MEMADD_SIZE_8BIT, sizeof(seek_buf));
-
-	while(LL_GPIO_IsInputPinSet(STC_GPIO_Port, STC_Pin) != RESET){}
+	while(LL_GPIO_IsInputPinSet(STC_GPIO_Port, STC_Pin) != RESET);
 
 	__enable_irq();
-	//シーク後はGPIO2をハイZにしておく
-	gpio_buf[1] = GPIO_INT_OFF & 0xff;
-	StreamI2C_Mem_Write(I2Cx, RADIO_ADDR_PUSH, gpio_buf, REG_GPIO, I2C_MEMADD_SIZE_8BIT, sizeof(gpio_buf));
+
+	//After the seek is completed GPIO2 Reset
+	RadioReg.GpioSet = (RadioReg.GpioSet & ~GPIO2_INT_MASK) | GPIO2_INT_OFF;
+	SetRegister(I2Cx, REG_GPIO, RadioReg.GpioSet);
+}
+void RadioTune(I2C_TypeDef *I2Cx,uint16_t chan)
+{
+	RadioReg.Tuning = (RadioReg.Tuning & ~(CHAN_WRITE_MASK | FLG_TUNE)) | (chan << CHAN_SHIFT) | FLG_TUNE;
+
+	SetRegister(I2Cx, REG_TUNING, RadioReg.Tuning);
+}
+uint16_t GetRegister(I2C_TypeDef *I2Cx,uint8_t Reg)
+{
+	uint8_t Read[2];
+	uint16_t ReadValue;
+
+	I2C_Mem_Read(I2Cx, RADIO_ADDR_PUSH, Read, Reg, I2C_MEMADD_SIZE_8BIT, sizeof(Read));
+
+	ReadValue = (Read[0] << 8) | (Read[1] & 0xFF);
+	return ReadValue;
 }
 uint16_t GetChan(I2C_TypeDef *I2Cx)
 {
-	uint8_t Read[2];
 	uint16_t ReadFreq;
 
-	I2C_Mem_Read(I2Cx, RADIO_ADDR_PUSH, Read, REG_STATUS, I2C_MEMADD_SIZE_8BIT, sizeof(Read));
-	ReadFreq = ((Read[0] << 8) | (Read[1] & 0xFF)) & CHAN_MASK;
-
-	//算出式はRadio.hを参照
-	//ReadFreq = (read_freq * 10 )+ 7600なので両辺を10で割ってReadFreq+760
+	//実数ではなく整数として取得する
+	ReadFreq = GetRegister(I2Cx, REG_STATUS);
+	ReadFreq &= CHAN_READ_MASK;
 	ReadFreq += 760;
 
 	return ReadFreq;
 }
 uint8_t GetRSSI(I2C_TypeDef *I2Cx)
 {
-	uint8_t Rssi[2];
-	uint16_t RssiValue;
+	uint16_t ReadRssi;
 
-	I2C_Mem_Read(I2Cx, RADIO_ADDR_PUSH, Rssi, REG_RSSI, I2C_MEMADD_SIZE_8BIT, sizeof(Rssi));
-	RssiValue = ((Rssi[0] << 8) | (Rssi[1] & 0xFF)) & RSSI_MASK;
-	RssiValue >>= RSSI_SHIFT;
+	ReadRssi = GetRegister(I2Cx, REG_RSSI);
+	ReadRssi &= RSSI_MASK;
+	ReadRssi >>= RSSI_SHIFT;
 
-	return (uint8_t)RssiValue;
+	return (uint8_t)ReadRssi;
 }
